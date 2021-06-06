8870743807b6f29fcb22dbedc8fa08721b58ab49
manecipoglu <muammernecipoglu@gmail.com>
2021-06-05 19:01:15 -0700

    implement ProductDetail page

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -1,11 +1,22 @@
+import { useParams } from "react-router-dom";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
+import PageNotFound from "./PageNotFound";
+
 export default function ProductDetail() {
+  const { id } = useParams();
+  const { data: product, error, loading } = useFetch(`products/${id}`);
+
+  if (loading) return <Spinner />;
+  if (!product) return <PageNotFound />;
+  if (error) throw error;
+
   return (
-    <h1> Detail </h1>
-    // <div id="detail">
-    //   <h1>{product.name}</h1>
-    //   <p>{product.description}</p>
-    //   <p id="price">${product.price}</p>
-    //   <img src={`/images/${product.image}`} alt={product.category} />
-    // </div>
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
   );
 }
