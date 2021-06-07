373c2c8e0b30f5d5804a0d85780229c14780ba3c
manecipoglu <muammernecipoglu@gmail.com>
2021-06-06 18:59:22 -0700

    add useRef to keep track of mounted & unmounted to prevent memory leak

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,29 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
+  const isMountedRef = useRef(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    isMountedRef.current = true;
     async function init() {
       try {
         const res = await fetch(baseUrl + url);
         if (res.ok) {
           const json = await res.json();
-          setData(json);
+          if (isMountedRef.current) setData(json);
         } else {
           throw res;
         }
       } catch (err) {
-        setError(err);
+        if (isMountedRef.current) setError(err);
       } finally {
-        setLoading(false);
+        if (isMountedRef.current) setLoading(false);
       }
     }
     init();
+
+    return () => {
+      isMountedRef.current = false;
+    };
   }, [url]);
 
   return { data, error, loading };
